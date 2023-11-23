d2f12f5b07e8fc52ac5041dda38de5e8646fc56c
Ajay Sommi <ajaysommi7@gmail.com>
2023-11-23 17:30:37 -0500

    added a main file

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+
+from pygame.examples.moveit import WIDTH
+from pygame.examples.moveit import HEIGHT
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
