4d4a5dfae00ef16bd22dac74eb2a01b1dd0c025d
Ajay Sommi <ajaysommi7@gmail.com>
2023-11-26 21:30:24 -0500

    changed main file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from constants import *
 import sys
 
 from pygame.examples.moveit import WIDTH
@@ -6,4 +7,12 @@ from pygame.examples.moveit import HEIGHT
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sudoku")
 
+screen.fill(BG_COLOR)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
